e15b3566c30da819c91daeb0b18bbb84b48fce13
Thomas COUSIN <thomas.cousin@gmail.com>
2021-09-23 08:47:55 +0000

    Add feature

---
+++ copilot.js
@@ -0,0 +1,15 @@
+// list european countries and their capitals
+var countries = {
+    "Belgium": "Brussels",
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Spain": "Madrid",
+    "United Kingdom": "London",
+    "United States": "Washington",
+    "Italy": "Rome",
+    "Switzerland": "Bern",
+    "Austria": "Vienna"
+  };
+
+
+
